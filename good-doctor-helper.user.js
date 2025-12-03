b4fd77f7edfc9a673a74484482f197a14b529035
Miiky <33239206+miiky-nerm@users.noreply.github.com>
2025-12-03 11:31:56 +0800

    Update version and author information in script

--- good-doctor-helper.user.js
+++ good-doctor-helper.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         🥇【好医生小助手】无人值守|自动静音|视频助手|考试助手|解禁调试
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      1.1.2
 // @description  ❌倍速播放✅屏蔽广告✅解禁调试✅视频助手✅考试助手(遍历试错)✅双模选择：只看不考、全看遂考🚑如果要与我交流，则需移步到下载本脚本的页面，在“反馈”区留下意见或直接私信我。
-// @author       Miiky
+// @author       原创作者：Dr.S 二创作者：境界程序员 三创作者：Miiky
 // @license      MIT License
 // @match        https://www.cmechina.net/cme/*
 // @match        https://www.cmechina.net/cme/exam.jsp*
@@ -16,7 +16,6 @@
 // @grant        GM_getValue
 // @run-at       document-idle
 // @grant        unsafeWindow
-// @antifeature  Donate听说含捐赠功能需要添加此代码（无任何作用）
 // @downloadURL https://raw.githubusercontent.com/miiky-nerm/good-doctor-helper/main/good-doctor-helper.user.js
 // @updateURL https://raw.githubusercontent.com/miiky-nerm/good-doctor-helper/main/good-doctor-helper.user.js
 // ==/UserScript==
